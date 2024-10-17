2bf217c562f22cc64a5d590156ebd818fabe5d25
soumya1801 <soumya.mishra1801@gmail.com>
2024-10-17 15:13:55 +0530

    DSA Practice

---
+++ DSA/Most Frequent Element/Source.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Source {
+
+ public static int mostFrequentElement(int[] arr) {
+
+    Map<Integer, Integer> frequencyMap = new HashMap<>();
+     for (int num : arr) {
+            if (frequencyMap.containsKey(num)) {
+                frequencyMap.put(num, frequencyMap.get(num) + 1);
+            } else {
+                frequencyMap.put(num, 1);
+            }
+        }
+        int maxFrequency = 0;
+        int mostFrequentElement = -1;
+
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() > maxFrequency) {
+                maxFrequency = entry.getValue();
+                mostFrequentElement = entry.getKey();
+            }
+        }
+
+        return mostFrequentElement;
+
+ }
+
+ public static void main(String[] args) {
+    int n;
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    int arr[] = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = sc.nextInt();
+    }
+    System.out.println(mostFrequentElement(arr));
+ }
+}
+
